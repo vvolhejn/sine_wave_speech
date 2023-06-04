22cd5fab35e916ea77e55bf3841879f48cc767a8
Václav Volhejn <vaclav.volhejn@gmail.com>
2023-06-04 12:31:47 +0200

    Remove template leftovers

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -2,10 +2,6 @@
 import { onMounted, ref } from 'vue'
 import sentenceSineWave from '../assets/sentence-sine-wave.wav'
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-
 const audioContext = new window.AudioContext()
 
 // These get set via the ref="..." attribute in the template
@@ -45,28 +41,8 @@ const onClick = () => {
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>Sine Wave Speech</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
   <audio :src="sentenceSineWave" ref="audioElement" @ended="onAudioEnded"></audio>
   <button
     data-playing="false"
