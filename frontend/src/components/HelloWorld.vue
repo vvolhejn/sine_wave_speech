ef8b8bb706d856436239f0644b9333ca3d55a902
Václav Volhejn <vaclav.volhejn@gmail.com>
2023-06-04 14:33:58 +0200

    Play SWS from JSON in frontend

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import sentenceSineWave from '../assets/sentence-sine-wave.wav'
+import sentence from '../assets/sentence-original.wav'
+import swsData from '../assets/sentence-sine-wave.json'
 
 const audioContext = new window.AudioContext()
 
@@ -38,12 +39,57 @@ const onClick = () => {
     isPlaying.value = false
   }
 }
+
+const playSineWaveSpeech = (time: number) => {
+  const oscillators = new Array<OscillatorNode>()
+  const gains = new Array<GainNode>()
+  const nWaves = swsData.frequencies[0].length
+  const nTimesteps = swsData.frequencies.length
+
+  for (let i = 0; i < nWaves; i++) {
+    const osc = new OscillatorNode(audioContext, {
+      frequency: swsData.frequencies[0][i],
+      type: 'sine',
+    })
+    // swsData.magnitudes[0][i]
+    const gain = new GainNode(audioContext, { gain: 0 })
+    osc.connect(gain).connect(audioContext.destination)
+    oscillators.push(osc)
+    gains.push(gain)
+  }
+
+  // Check if context is in suspended state (autoplay policy)
+  if (audioContext.state === 'suspended') {
+    audioContext.resume()
+  }
+  const secondsPerTimestep = swsData.hopSize / swsData.sr
+
+  for (let t = 0; t < nTimesteps; t++) {
+    for (let i = 0; i < nWaves; i++) {
+      const osc = oscillators[i]
+      const gain = gains[i]
+      osc.frequency.linearRampToValueAtTime(
+        swsData.frequencies[t][i],
+        time + t * secondsPerTimestep
+      )
+      gain.gain.linearRampToValueAtTime(
+        swsData.magnitudes[t][i],
+        time + t * secondsPerTimestep
+      )
+    }
+  }
+
+  oscillators.forEach((oscillator) => {
+    oscillator.start(time)
+    oscillator.stop(time + nTimesteps * secondsPerTimestep)
+  })
+}
 </script>
 
 <template>
   <h1>Sine Wave Speech</h1>
 
-  <audio :src="sentenceSineWave" ref="audioElement" @ended="onAudioEnded"></audio>
+  <audio :src="sentence" ref="audioElement" @ended="onAudioEnded"></audio>
   <button
     data-playing="false"
     role="switch"
@@ -51,7 +97,10 @@ const onClick = () => {
     @click="onClick"
     ref="playButton"
   >
-    <span>Play/Pause</span>
+    <span>Play original</span>
+  </button>
+  <button @click="playSineWaveSpeech(audioContext.currentTime)">
+    Play Sine Wave Speech
   </button>
 </template>
 
