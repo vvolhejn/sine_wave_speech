cde08734ff6943aa8e8d69ab74871120fc768778
Václav Volhejn <vaclav.volhejn@gmail.com>
2023-06-04 12:30:57 +0200

    Add basic audio playback

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,9 +1,47 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import sentenceSineWave from '../assets/sentence-sine-wave.wav'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
+const audioContext = new window.AudioContext()
+
+// These get set via the ref="..." attribute in the template
+const audioElement = ref<HTMLAudioElement | null>(null)
+const playButton = ref<HTMLButtonElement | null>(null)
+
+const isPlaying = ref(false)
+
+onMounted(() => {
+  if (!audioElement.value) return
+
+  const track = audioContext.createMediaElementSource(audioElement.value)
+  track.connect(audioContext.destination)
+})
+
+const onAudioEnded = () => {
+  isPlaying.value = false
+}
+
+const onClick = () => {
+  // Check if context is in suspended state (autoplay policy)
+  if (audioContext.state === 'suspended') {
+    audioContext.resume()
+  }
+
+  if (!audioElement.value) return
+
+  // Play or pause track depending on state
+  if (!isPlaying.value) {
+    audioElement.value.play()
+    isPlaying.value = true
+  } else {
+    audioElement.value.pause()
+    isPlaying.value = false
+  }
+}
 </script>
 
 <template>
@@ -29,6 +67,16 @@ const count = ref(0)
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <audio :src="sentenceSineWave" ref="audioElement" @ended="onAudioEnded"></audio>
+  <button
+    data-playing="false"
+    role="switch"
+    aria-checked="false"
+    @click="onClick"
+    ref="playButton"
+  >
+    <span>Play/Pause</span>
+  </button>
 </template>
 
 <style scoped>
