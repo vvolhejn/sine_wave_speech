f2a2e0cd2663114487fa88ba0d6ac897e118f506
Václav Volhejn <vaclav.volhejn@gmail.com>
2024-10-29 21:59:59 +0100

    Improve compilation scripts

--- wasm_realtime_sws/compile_to_wasm.sh
+++ wasm_realtime_sws/compile_to_wasm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eo pipefail
+
 cd "$(dirname "$0")" # cd to script dir
 
 wasm-pack build --target web
@@ -7,5 +9,7 @@ wasm-pack build --target web
 output_dir=$(realpath "../frontend/src/wasm_realtime_sws")
 
 cp -R ./pkg/ $output_dir
+rm $output_dir/.gitignore # We do want to track the wasm files
+rm $output_dir/README.md  # The README is just a copy of the one here
 
 echo "Wasm compiled to $output_dir"
--- wasm_realtime_sws/live_compile.sh
+++ wasm_realtime_sws/live_compile.sh
@@ -2,6 +2,9 @@
 
 cd "$(dirname "$0")" # cd to script dir
 
+clear
+./compile_to_wasm.sh
+
 fswatch src/ --include '.rs$' | while read -r changed_file; do
     clear
     echo "Change detected in: $changed_file"
